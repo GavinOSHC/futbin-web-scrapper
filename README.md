46b5e620b2aef146e68ccb3069176c85462fcce3
gavin <gavinoshc@gmail.com>
2020-08-29 20:26:37 +0100

    more data scrapped and changed readme

--- README.md
+++ README.md
@@ -2,6 +2,80 @@
 
 ### Scrapes the stats of a player from futbin using puppeteer
 
-#### url = player you want to scrape
+#### How To use
 
-#### More data to come
+call the getFutbin function with the url of your player e.g getFutbin("https://www.futbin.com/20/player/48200/riyad-mahrez");
+
+```javascript
+async function getFutbin(url) {
+    ...
+}
+```
+
+The function will return a player object which will look like this
+
+```javascript
+[
+  'Riyad Mahrez - TOTS': {
+    info: {
+      Name: 'Riyad Mahrez',
+      Club: ' Manchester City',
+      Nation: ' Algeria',
+      League: ' Premier League',
+      Skills: '5 ',
+      'Weak Foot': '4 ',
+      'Intl. Rep ': '3 ',
+      'Foot ': 'Left',
+      'Height ': `179cm | 5'10"`,
+      'Weight ': '67',
+      Revision: 'TOTS',
+      'Def. WR': 'Med',
+      'Att. WR': 'Med',
+      'Added on': '2020-05-01',
+      Origin: 'TOTSSF-PL',
+      'R.Face ': '',
+      'B.Type': 'Lean',
+      Age: '29 years old'
+    },
+    stats: {
+      PACE: 95,
+      Acceleration: 98,
+      'Sprint Speed': 92,
+      SHOOTING: 94,
+      Positioning: 95,
+      Finishing: 92,
+      'Shot Power': 95,
+      'Long Shots': 96,
+      Volleys: 91,
+      Penalties: 84,
+      PASSING: 95,
+      Vision: 97,
+      Crossing: 97,
+      'FK. Accuracy': 93,
+      'Short Passing': 93,
+      'Long Passing': 90,
+      Curve: 99,
+      DRIBBLING: 97,
+      Agility: 99,
+      Balance: 94,
+      Reactions: 83,
+      'Ball Control': 98,
+      Dribbling: 98,
+      Composure: 91,
+      DEFENDING: 50,
+      Interceptions: 51,
+      'Heading Accuracy': 63,
+      'Def. Awareness': 59,
+      'Standing Tackle': 40,
+      'Sliding Tackle': 28,
+      PHYSICALITY: 77,
+      Jumping: 78,
+      Stamina: 99,
+      Strength: 70,
+      Aggression: 62
+    },
+    Price: '116,000 ',
+    'Last Updated': 2020-08-29T19:21:50.151Z
+  }
+]
+```
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const puppeteer = require("puppeteer");
 
-async function getFutbin() {
-  const url = "https://www.futbin.com/20/player/48200/riyad-mahrez";
-
+async function getFutbin(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle2" });
@@ -10,33 +8,60 @@ async function getFutbin() {
   const data = await page.evaluate(() => {
     let price = document.querySelector('span[id="ps-lowest-1"]').innerText;
     let stats = document.querySelector('div[class="card-body"]').innerText;
+    let infoHeading = [
+      ...document.querySelectorAll(
+        'div[id="info_content"] > table > tbody > tr > th'
+      ),
+    ];
 
-    return { price, stats };
+    let infoValues = [
+      ...document.querySelectorAll(
+        'div[id="info_content"] > table > tbody > tr > td'
+      ),
+    ];
+
+    const infoHeadingArray = infoHeading.map((element) => element.innerText);
+    const infoValuesArray = infoValues.map((element) => element.innerText);
+
+    return { price, stats, infoHeadingArray, infoValuesArray };
   });
 
   const index = data.stats.indexOf("Physical");
 
   const stats = data.stats.slice(0, index);
-  const price = data.price;
-
   const statArray = stats.split("\n");
 
-  var playerObject = {};
+  var playerObject = {
+    info: {},
+    stats: {},
+  };
 
   for (let i = 0; i < statArray.length; i++) {
     if (statArray[i] != "") {
       if (!Number(statArray[i])) {
-        playerObject[statArray[i]] = Number(statArray[i + 1]);
+        playerObject.stats[statArray[i]] = Number(statArray[i + 1]);
       }
     }
   }
 
-  playerObject["Price"] = price;
-  playerObject["Last Price Updated"] = new Date();
+  for (let i = 0; i < data.infoHeadingArray.length; i++) {
+    if (data.infoHeadingArray[i] !== "") {
+      playerObject.info[data.infoHeadingArray[i]] = data.infoValuesArray[i];
+    }
+  }
 
-  console.log(playerObject);
+  playerObject["Price"] = data.price;
+  playerObject["Last Updated"] = new Date();
+
+  const playerNameAndtype = `${playerObject.info.Name} - ${playerObject.info.Revision}`;
+
+  var player = [];
+
+  player[playerNameAndtype] = playerObject;
+
+  console.log(player);
 
   return await browser.close();
 }
 
-getFutbin();
+getFutbin("https://www.futbin.com/20/player/48200/riyad-mahrez");
